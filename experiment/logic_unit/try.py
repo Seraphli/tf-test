df589df898963153516135af11d8a3998ae8f9c9
SErAphLi <seraphlivery@gmail.com>
2017-06-10 16:30:37 +0800

    again can not learn, may be one unit is not enough

--- experiment/logic_unit/try.py
+++ experiment/logic_unit/try.py
@@ -18,10 +18,11 @@ loss = tf.reduce_mean((y_ - y) ** 2)
 opt = tf.train.AdamOptimizer(learning_rate=0.001)
 train_op = opt.minimize(loss)
 sess.run(tf.global_variables_initializer())
+
+for t in trange(10000):
+    batch_x = np.array([np.random.randint(2, size=8) for _ in range(18)] + [[1, 1, 1, 1, 1, 1, 1, 1] for _ in range(14)])
+    batch_y_ = np.array([[1] if sum(batch_x[_]) == 8 else [0] for _ in range(18)] + [[1] for _ in range(14)])
+    l, to = sess.run([loss, train_op], feed_dict={x: batch_x, y_: batch_y_})
+    # print(l)
 result = sess.run(y, feed_dict={x: [[1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 0]]})
 print(result)
-for t in trange(10000):
-    batch_x = np.array([np.random.randint(2, size=8) for _ in range(32)])
-    batch_y_ = np.array([[1] if sum(batch_x[_]) == 8 else [0] for _ in range(32)])
-    loss, to = sess.run([loss, train_op], feed_dict={x: batch_x, y_: batch_y_})
-    print(loss)
