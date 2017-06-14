8026590853c382b75d9a5baf60e5cf8de3c287f9
seraphli <seraphlivery@gmail.com>
2017-06-14 11:09:30 +0800

    use total step or scale

--- experiment/epsilon_bounce/epsilon.py
+++ experiment/epsilon_bounce/epsilon.py
@@ -2,13 +2,23 @@ import numpy as np
 
 
 class BounceEpsilon(object):
-    def __init__(self, init, final, loss, scale, gravity):
+    def __init__(self, init, final, loss, gravity, total=None, scale=None):
         self._init = init
         self._final = final
+        self._total = total
         self._loss = loss
         self._scale = scale
         self._gravity = gravity
+        self._eps = 0.001
         self._height = self._init - self._final
+        if self._scale == None:
+            if self._total == None:
+                self._scale = 1
+            else:
+                n = np.log(self._eps / self._height) / np.log(self._loss)
+                sqrt_l = np.sqrt(self._loss)
+                self._scale = self._total / (2 * np.sqrt(2 * self._height / self._gravity) *
+                                             (sqrt_l * (1 - sqrt_l ** n) / (1 - sqrt_l) + 0.5))
         self.reset()
 
     def reset(self):
@@ -27,7 +37,7 @@ class BounceEpsilon(object):
         self._t += 1
         if not self._is_terminate and self._t / self._scale > self._staged_t:
             self._height *= self._loss
-            if self._height < 0.001:
+            if self._height < self._eps:
                 self._is_terminate = True
                 self._h_func = lambda: self._final
             self._stage_t = np.sqrt(2 * self._height / self._gravity)
@@ -37,9 +47,9 @@ class BounceEpsilon(object):
 
 
 if __name__ == '__main__':
-    be = BounceEpsilon(1.0, 0.01, 0.7, 20, 0.0001)
+    be = BounceEpsilon(1.0, 0.01, 0.7, 0.0001, total=20000)
     heights = []
-    for _ in range(50000):
+    for _ in range(20000):
         heights.append(be.step())
     print(heights)
     with open('height.csv', 'w') as f:
